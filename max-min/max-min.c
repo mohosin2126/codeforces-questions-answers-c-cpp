92e7bb86c515a631ddb1147c1a3dd0a554a08b3d
mohosin2126 <mohosin2126@gmail.com>
2024-10-06 02:10:58 +0600

    fix the issue

--- max-min/max-min.c
+++ max-min/max-min.c
@@ -3,19 +3,20 @@
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
+
+    // Find and print the minimum number
+    if (a <= b && a <= c) {
+        printf("%d ", a);
+    } else if (b <= a && b <= c) {
+        printf("%d ", b);
+    } else {
+        printf("%d ", c);
+    }
+
+    // Find and print the maximum number
     if (a >= b && a >= c) {
         printf("%d", a);
     } else if (b >= a && b >= c) {
-        printf("%d", b);
-    } else {
-        printf("%d", c);
-    }
-
-    printf(" ");
-
-    if (a <= b && a <= c) {
-        printf("%d", a);
-    } else if (b <= a && b <= c) {
         printf("%d", b);
     } else {
         printf("%d", c);
