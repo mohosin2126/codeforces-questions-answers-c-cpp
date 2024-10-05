02cabe1580fdbd8ce476898a9373549fea561f39
mohosin2126 <mohosin2126@gmail.com>
2024-10-06 01:55:27 +0600

    digit or letter if letter small or capital

---
+++ capital-small-digit/capital-small-digit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+    char ch;
+    scanf("%c", &ch);
+    if(ch >= '0' && ch <= '9'){
+        printf("IS DIGIT\n");
+    }
+    else {
+        printf("ALPHA\n");
+        if(ch >= 'A' && ch <= 'Z'){
+            printf("IS CAPITAL\n");
+        }
+        else if(ch >= 'a' && ch <= 'z'){
+            printf("IS SMALL\n");
+        }
+    }
+    
+    return 0;
+}
