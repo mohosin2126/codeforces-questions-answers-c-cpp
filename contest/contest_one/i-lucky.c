5198a08bfe91412e5a81caad3eecec4920158b6a
mohosin2126 <mohosin2126@gmail.com>
2024-10-10 22:57:17 +0600

    solved lucky number

---
+++ contest/contest_one/i-lucky.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+     
+int n;
+scanf("%d",&n);
+int firstNumber=n%10;
+int secondNumber=n/10;
+
+if(firstNumber%secondNumber==0||secondNumber%firstNumber==0){
+    printf("YES\n");
+}
+else{
+    printf("NO\n");
+}
+    return 0;
+}
