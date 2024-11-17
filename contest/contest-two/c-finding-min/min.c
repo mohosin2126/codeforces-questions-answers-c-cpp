42a3096b4766e215b7bcf4a657686e2ca66c5698
mohosin2126 <mohosin2126@gmail.com>
2024-11-17 20:03:27 +0600

    c-finding number has been solved

---
+++ contest/contest-two/c-finding-min/min.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+    for (int i=0;i<x;i++){
+        for (int j=0;j<y;j++){
+            int n;
+            scanf('%d',&n);
+        }
+    }
+    printf("%d",x);
+    return 0;
+}
