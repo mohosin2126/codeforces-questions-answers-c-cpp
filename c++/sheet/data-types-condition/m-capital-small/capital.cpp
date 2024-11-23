628c827be936c58df567657c6cfe96c9065543a1
mohosin2126 <mohosin2126@gmail.com>
2024-11-23 12:29:41 +0600

    m-capital small or digit has been solved

--- c++/sheet/data-types-condition/m-capital-small/capital.cpp
+++ c++/sheet/data-types-condition/m-capital-small/capital.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-    
+    char c;
+    cin>>c;
+    if(c>='0' && c<='9'){
+        cout<<"IS DIGIT";
+    }
+    else{
+        cout<<"ALPHA"<<endl;
+        if(c>='A' && c<='Z'){
+             cout<<"IS CAPITAL"<<endl;
+        }
+        else if(c>='a' && c<='z'){
+            cout<<"IS SMALL";
+        }
+    }
     return 0;
 }
